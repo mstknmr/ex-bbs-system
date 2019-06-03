fa58d4d4f42cfa49cccaa8100970d2168a611673
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-03 11:11:44 +0900

    articleRepositoryのfindAll()作成

--- document/sql/showAll.sql
+++ document/sql/showAll.sql
@@ -1,4 +1,6 @@
 SELECT id,name,content FROM articles;
 SELECT id,name,content,article_id FROM comments;
 
-SELECT a.id,a.name,a.content,c.id,c.name,c.content FROM articles a join comments c on c.article_id=a.id;
+SELECT
+a.id as article_id,a.name as article_name,a.content as article_content,
+c.id as comment_id,c.name as comment_name,c.content as comment_conetnt FROM articles a join comments c on c.article_id=a.id;
--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,5 +1,32 @@
 package com.example.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+
+@Repository
 public class ArticleRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private final static RowMapper<Article> ARTICLE_ROW_MAPPER=(rs,i)->{
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+	
+	public List<Article> findAll(){
+		String findAllSql = "SELECT id,name,content FROM articles;";
+		List<Article> articleList = template.query(findAllSql, ARTICLE_ROW_MAPPER);
+		return articleList;
+		
+	}
 
 }
